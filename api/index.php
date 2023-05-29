061f1f1d4da815ccceeae9d0193df4be5dadcb33
Gui D'Haene <gui@sparkle.tech>
2023-05-29 19:29:20 +0200

    .

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once($_SERVER['DOCUMENT_ROOT'] . '/api/config.php');
-  echo $_SERVER['DOCUMENT_ROOT'];
-  echo $host;
-  echo $port;
-  echo $username;
-  echo $dbname;
-  echo $password;
-?>
