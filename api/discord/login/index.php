44713b07aabe3febe714d5a24c6253be108e9d72
Gui D'Haene <gui@sparkle.tech>
2023-05-29 17:59:06 +0200

    update login api

--- api/discord/login/index.php
+++ api/discord/login/index.php
@@ -17,23 +17,27 @@ $payload = [
     'client_id' => '1111955711665127535',
     'client_secret' => 'fhHNfcMuDa4VaItocUqC7ipqN_CAOKPB',
     'grant_type' => 'authorization_code',
-    'redirect_uri' => 'https://beta.gp-prognostics.fr/login',
+    'redirect_uri' => 'https://beta.gp-prognostics.fr/login/',
     'scope' => 'identify'
 ];
-echo json_encode($payload);
 
-$playload_string = http_build_query($payload);
-$discord_token_url = 'https://discordapp.com/api/oauth2/token';
+$curl = curl_init();
 
-$ch = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://discordapp.com/api/oauth2/token',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => http_build_query($payload),
+  CURLOPT_HTTPHEADER => array(
+    'Accept: application/json',
+    'Content-Type: application/x-www-form-urlencoded',
+  ),
+));
 
-curl_setopt($ch, CURLOPT_URL, $discord_token_url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $playload_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
 
-$response = curl_exec($ch);
-curl_close($ch);
+curl_close($curl);
 echo $response;
 
 ?>
