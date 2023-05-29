04ffda4ff5e3a24e60d98fee05eb71e3b0b2db3e
Gui D'Haene <gui@sparkle.tech>
2023-05-29 18:53:47 +0200

    add php infos

--- api/index.php
+++ api/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/api/config.php');
-echo $host;
-echo $port;
-echo $username;
-echo $dbname;
-echo $password;
+  require_once($_SERVER['DOCUMENT_ROOT'] . 'beta.gp-prognostics.fr/api/config.php');
+  echo $host;
+  echo $port;
+  echo $username;
+  echo $dbname;
+  echo $password;
 ?>
