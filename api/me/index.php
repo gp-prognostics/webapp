341580475f71213c027ec77b13d795fbb751314c
Gui D'Haene <gui@sparkle.tech>
2023-06-02 14:40:57 +0200

    Add /me api and call in home page

---
+++ api/me/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/api/config.php');
+
+$pdo = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $username, $password);
+
+$headers = apache_request_headers();
+$token = $headers['Authorization'];
+
+$stmt = $pdo->prepare("SELECT username, avatarUrl FROM users WHERE token = :token");
+$stmt->bindParam(':token', $token);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user) {
+    echo json_encode(
+        array(
+            'username' => $user['username'],
+            'avatarUrl' => $user['avatarUrl']
+        )
+    );
+} else {
+    echo json_encode(array('error' => 'User not found'));
+}
+
+
+?>
